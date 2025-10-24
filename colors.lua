d05e7f36e514961e4b19c6588d4b07f73e3b3881
William Parsons-Heins <william.parsons-heins@expanamarkets.com>
2025-10-24 09:26:56 +0100

    add to color_schemes

--- colors.lua
+++ colors.lua
@@ -3,8 +3,11 @@ local wezterm = require("wezterm")
 local gruvbox = wezterm.color.get_builtin_schemes()["Gruvbox Material (Gogh)"]
 gruvbox.background = "#151818"
 
+local kanagawa = require("colors.kanagawa")
+
 return {
 	color_schemes = {
 		["Gruvbox Material Hard (Gogh)"] = gruvbox,
+		["kanagawa"] = kanagawa,
 	},
 }
